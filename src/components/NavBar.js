d79b7de383146f7153f4ec673ce090a96eddc3ca
JoaquinBonet <jnbonet@gmail.com>
2021-05-28 19:47:03 -0300

    subo NavBar responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,24 +10,28 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 
 export const NavBar = () => {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="dark" variant="dark" expand="lg">
             <Navbar.Brand href="#home" className="font-link">La cueva del guitarrista</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="#cables">Cables</Nav.Link>
-                <Nav.Link href="#features">Pedales</Nav.Link>
-                <Nav.Link href="#pricing">Multi-fx</Nav.Link>
-                <NavDropdown title="Otros accesorios" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Púas</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Mics</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Correas</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Otros</NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
-            <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-info">Search</Button>
-            </Form>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#cables">Cables</Nav.Link>
+                    <Nav.Link href="#features">Pedales</Nav.Link>
+                    <Nav.Link href="#pricing">Multi-fx</Nav.Link>
+                    <NavDropdown title="Otros accesorios" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Púas</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Mics</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Correas</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Otros</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+
+            </Navbar.Collapse>
         </Navbar>
     );
 }
