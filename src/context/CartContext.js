7155f24e6413c4a172badbcb673b49c3696d053d
JoaquinBonet <jnbonet@gmail.com>
2021-06-26 16:13:21 -0300

    Corrijo context

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -4,29 +4,35 @@ export const CartContext = React.createContext();
 export const useCartContext = () => React.useContext(CartContext);
 
 export const CartComponentContext = props => {
+
     const [cart, setCart] = React.useState([]);
 
     const isInCart = (id) => {
         return cart.find(item => { return item.item.id === id });
     }
 
-    const updateRepeatedItem = (item) => {
-       
-        const repeatedItem = isInCart(item.item.id)
-        const newQuantity = repeatedItem.quantity + item.quantity;
-        const updateItem = {item, quantity: newQuantity};
-        const index = cart.indexOf(repeatedItem);
-        cart.splice(index, 1)
-        return updateItem;
-    }
-
-   
-
     const addItem = (item) => {
 
-        return isInCart(item.item.id) ? setCart([...cart, updateRepeatedItem(item)]) : setCart([...cart, item]);
+        return isInCart(item.item.id) ? updateRepeatedItem(item) : setCart([...cart, item]);
 
     }
+
+    const updateRepeatedItem = (item) => {
+
+        const updatedItem = cart.map(element => {
+            if (element.item.id === item.item.id) {
+                return { ...element, quantity: item.quantity + element.quantity }
+            }
+            return element;
+        });
+
+        return setCart(updatedItem);
+        
+    }
+
+
+
+
 
     const removeItem = (id) => {
         return cart.filter(item => { return item.id !== id });
@@ -36,7 +42,7 @@ export const CartComponentContext = props => {
     const clear = () => {
         setCart([]);
     }
-   
+
     console.log(cart)
     return <CartContext.Provider value={{ cart, addItem, removeItem, clear }}>
         {props.children}
